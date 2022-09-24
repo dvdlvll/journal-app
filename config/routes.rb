2fa57f0bc9842e8810938ef5468768d89e922450
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-09-24 10:02:17 +0800

    bootstrapified application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get "home/index"
+
   root "home#index"
 
   resources :categories
   resources :tasks
+
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
