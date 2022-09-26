c856055d40fb97ce7ff31070487758b9710fe0fd
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-09-26 13:20:21 +0800

    added devise gem functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     validates :name, presence: true, uniqueness: true
-    has_many :tasks
+    
+    has_many :tasks, dependent: :destroy 
+    belongs_to :user
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
     validates :category_id, presence: true
+    
     belongs_to :category
+    belongs_to :user
 end
