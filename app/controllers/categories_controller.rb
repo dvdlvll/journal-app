5d101892e49d0c6abffc266393b2312829aacb31
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-09-27 16:33:02 +0800

    finalized project

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories or /categories.json
   def index
     @categories = Category.where(user_id: current_user.id).order("name DESC") if user_signed_in?
+
   end
 
   # GET /categories/1 or /categories/1.json
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     validates :name, presence: true
+    validates :user_id, presence: true
     
     has_many :tasks, dependent: :destroy 
     belongs_to :user
