bbac18a80490f86ec0989209b96edc643faa327a
Dave Lavilla <davelavilla@gmail.com>
2022-12-05 19:11:51 +0800

    host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "https://journal-app.up.railway.app"
+  config.hosts << "journal-app.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
